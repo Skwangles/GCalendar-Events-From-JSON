170d3d6f806918f12bae6b0bd1d76a153b0af665
Skwangles <tree_house@hotmail.co.nz>
2021-06-28 20:12:05 +1200

    implement wrapper

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), listEvents);
+  authorize(JSON.parse(content), listCalendars);
 });
 
 /**
@@ -96,4 +97,27 @@ function listEvents(auth) {
   });
 }
 
+
+function listCalendars(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+}
 //getC.listCalendars();
